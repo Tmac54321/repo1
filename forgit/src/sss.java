7b9f79e3b7706c5136a65d15a8007ee750014ce3
Tmac54321 <1007448003@qq.com>
2019-06-16 21:11:13 +0800

    添加test功臣给至git

---
+++ forgit/src/sss.java
@@ -0,0 +1,8 @@
+public class sss {
+    public static void main(String[] args) {
+        System.out.println("dfsjjaf");
+    }
+    public  void sau() {
+        System.out.println("dflkalk");
+    }
+}
